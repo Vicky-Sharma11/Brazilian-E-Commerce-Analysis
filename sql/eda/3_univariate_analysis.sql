16b0295644a586106e4f8e53f557cbeb9a45d5b2
Vicky-Sharma11 <vickysharma95825@gmail.com>
2026-09-26 17:31:59 +0530

    Update and expand unvariate analysis queries

--- sql/eda/3_univariate_analysis.sql
+++ sql/eda/3_univariate_analysis.sql
@@ -1,5 +1,4 @@
-
--- Orders by order status
+-- Order distribution by order status
 
 SELECT 
     order_status
@@ -37,26 +36,106 @@ FROM (
 -- There are only 2997 customes who placed more than single order
 -- and 93,099 with only one order
 
+
+-- Orders cancellation rate
+
+SELECT 
+ROUND(
+    COUNT(*) * 1.0 / (SELECT COUNT(*) FROM orders ) * 100
+    , 2) AS cancellation_rate
+FROM orders
+WHERE order_status = 'canceled' ;
+
 -- Late order percentage share
 
 SELECT 
     ROUND(
-        COUNT(*)-- * 1.0 / (SELECT COUNT(*) FROM orders WHERE delivered_to_customer_at IS NOT NULL) * 100.0
+        COUNT(*) * 1.0 / (SELECT COUNT(*) FROM orders WHERE order_status = 'delivered' ) * 100.0
         ,2)
 FROM orders
-WHERE delivered_to_customer_at IS NOT NULL
+WHERE order_status = 'delivered'
     AND DATE(delivered_to_customer_at) > DATE(estimated_delivery_timestamp) ;
 
--- 6,535 late deliveries, representing approximately 6.77% of delivered orders.
--- Note: Only orders with a recorded delivery date are considered. 
--- This can include orders with a canceled status if they were delivered before cancellation.
+-- 6,534 late deliveries, representing approximately 6.77% of delivered orders.
+
 
 -- Average time taken to deliver an order ( from purchase date to delivered date)
 
 SELECT 
     ROUND(
-        EXTRACT(EPOCH FROM AVG(delivered_to_customer_at - purchase_timestamp)) / 86400
+        EXTRACT(
+            EPOCH FROM AVG(delivered_to_customer_at - purchase_timestamp)
+            ) / 86400
         , 2) AS delivery_time
 FROM orders
-WHERE delivered_to_customer_at IS NOT NULL ;
+WHERE order_status = 'delivered' ;
+
+
+-- Product wise sales
+
+SELECT 
+    product_id
+    ,SUM(price) AS sales
+FROM order_items AS ot 
+INNER JOIN orders AS o
+    ON ot.order_id = o.order_id
+    AND o.order_status = 'delivered'
+GROUP BY product_id
+ORDER BY sales DESC ;
+
+-- Seller wise sales
+
+SELECT
+    seller_id
+    ,SUM(price) AS sales
+FROM order_items AS ot 
+INNER JOIN orders AS o
+    ON ot.order_id = o.order_id
+    AND o.order_status = 'delivered'
+GROUP BY seller_id 
+ORDER BY sales DESC ;
+
+-- Payment value by payment type
+
+SELECT 
+    payment_type
+    ,SUM(payment_value) AS total_payment_value
+FROM order_payments AS op 
+INNER JOIN orders AS o 
+    ON op.order_id = o.order_id
+    AND o.order_status = 'delivered'
+GROUP BY payment_type 
+ORDER BY total_payment_value DESC;
+
+-- Total revenue by states
+
+SELECT
+    c.customer_state
+    ,SUM(price + freight_value) AS total_revenue
+FROM customers  AS c 
+JOIN orders AS o 
+    ON c.customer_id = o.customer_id
+    AND o.order_status = 'delivered'
+JOIN order_items AS ot 
+    ON o.order_id = ot.order_id 
+GROUP BY c.customer_state
+ORDER BY total_revenue DESC ;
+
+-- Total revenue by category
+
+SELECT 
+    c.product_category_name_english
+    ,SUM(price + freight_value) AS total_revenue
+FROM products AS p
+JOIN product_category_name_translation AS c
+    ON c.product_category_name = p.product_category_name
+JOIN order_items AS ot
+    ON p.product_id = ot.product_id
+JOIN orders AS o
+    ON o.order_id = ot.order_id
+    AND o.order_status = 'delivered'
+GROUP BY c.product_category_name_english 
+ORDER BY total_revenue DESC ;
+
+
 
